9cfcc00ba63e53870ccb8dc834c33e47b3fddd6b
Kevin Efraim <kevinefraim02@gmail.com>
2021-12-24 10:19:25 -0300

    proyecto terminado - falta funcionalidad del carrusel

---
+++ src/components/cards/Cards.jsx
@@ -0,0 +1,55 @@
+import { Component } from "react";
+import CardsStyles from "./cards.css";
+import bartImg from "../../img/bart.jpg";
+import maggieImg from "../../img/maggie.jpg";
+import homerImg from "../../img/homer.jpg";
+
+class Cards extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="cards-container bg-warning ">
+        <div class="card">
+          <img class="card-img-top " src={maggieImg} />
+          <div class="card-body">
+            <h5 class="card-title">Maggie Simpson</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <button class="btn btn-danger">Go somewhere</button>
+          </div>
+        </div>
+
+        <div class="card">
+          <img class="card-img-top " src={homerImg} />
+          <div class="card-body">
+            <h5 class="card-title">Homero Simpson</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <button class="btn btn-danger">Go somewhere</button>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top " src={bartImg} />
+          <div class="card-body">
+            <h5 class="card-title">Bart Simpson</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <button class="btn btn-danger">Go somewhere</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Cards;
---
+++ src/components/carrusel/Carrusel.jsx
@@ -0,0 +1,36 @@
+import { Component } from "react";
+import headerStyles from "../header/header.css";
+import carrusel1 from "../../img/carrousel-1.jpg";
+import carrusel2 from "../../img/carrousel-2.jpg";
+import carrusel3 from "../../img/carrousel-3.jpg";
+
+class Carrusel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div
+        id="carouselExampleSlidesOnly"
+        className="carousel slide"
+        data-ride="carousel"
+      >
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img className="d-block w-100" src={carrusel3} />
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src={carrusel1} a />
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src={carrusel2} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carrusel;
